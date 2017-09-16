282de4bc9e4d611382bc33c8d5d89709c5877b84
Mark Weigl <markwco@icloud.com>
2017-09-16 12:02:11 -0600

    updated config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 exports.DATABASE_URL = process.env.MONGODB_URI ||
                        global.DATABASE_URL ||
                       'mongodb://localhost/culinaryDb';
+exports.CLIENT_ORIGIN=process.env(INIT_ORIGIN)                      
 exports.PORT = process.env.PORT || 8080;
