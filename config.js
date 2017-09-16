91fea0673033a63b425cf097515c1ff4f7b65a96
Mark Weigl <markwco@icloud.com>
2017-09-16 12:29:15 -0600

    changes to travis.yml

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ exports.DATABASE_URL = process.env.MONGODB_URI ||
                        global.DATABASE_URL ||
                       'mongodb://localhost/culinaryDb';
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 
-                        'https://netlify.com';                      
+                        'https://hairdresser-boar-22082.netlify.com/';                      
 exports.PORT = process.env.PORT || 8080;
