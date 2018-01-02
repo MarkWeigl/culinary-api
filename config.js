701510d5f35da4b8945faabd9c9e35f894184964
Patrick Ford <patrickford@msn.com>
2018-01-01 21:00:35 -0800

    model fixes plus PUT fixes

--- config.js
+++ config.js
@@ -5,4 +5,4 @@ exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN ||
                         'https://hairdresser-boar-22082.netlify.com/';                      
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
-exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY;
