26a0fbe9a4ead735b49426e1bf4f0d93683cb57f
Mark Weigl <markwco@icloud.com>
2018-02-07 20:50:09 -0700

    correctd client origin url in config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ exports.DATABASE_URL = process.env.MONGODB_URI ||
                        global.DATABASE_URL ||
                       'mongodb://localhost/culinaryDb';
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 
-                        'https://hairdresser-boar-22082.netlify.com/';                      
+                        'https://astronaut-phyllis-41658.netlify.com/';                      
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY;
